3710d11c55a0d180f906eade3ba814ec76b3ca35
Tim La <tlamarketing111@gmail.com>
2022-02-23 11:33:09 -0500

    created function to grab data for properties for sale & rent

--- pages/index.js
+++ pages/index.js
@@ -48,3 +48,9 @@ export default function Home() {
     </Box>
   )
 }
+
+
+export async function getStaticProps() {
+  const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose-for-sale&hitsPerPage=6`);
+  const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose-for-rent&hitsPerPage=6`);
+}
