481413204ca711da626d1dcd09d914f3b772fadc
Tim La <tlamarketing111@gmail.com>
2022-02-25 10:17:04 -0500

    added imports in app file

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
 // import '../styles/globals.css'
+import Router from 'next/router';
+import Head from 'next/head';
+import NProgress from 'nprogress';
+import { ChakraProvider } from '@chakra-ui/react'
+
+import Layout from '../components/Layout'
 
 
 function MyApp({ Component, pageProps }) {
