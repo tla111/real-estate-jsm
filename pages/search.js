0d84e8fffbcb0c6a928d93d79bda2fdd576c9b16
Tim La <tlamarketing111@gmail.com>
2022-02-28 16:42:06 -0500

    created search filter component

--- pages/search.js
+++ pages/search.js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import Image from 'next/image';
 import { Flex, Box, Text, Icon } from '@chakra-ui/react';
 import { BsFilter } from 'react-icons/bs';
+import SearchFilters from '../components/SearchFilters';
 
 const Search = () => {
     const [searchFilters, setSearchFilters] = useState(false);
