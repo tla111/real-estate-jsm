d2311da4a2597280b194418eee094d0d289145d1
Tim La <tlamarketing111@gmail.com>
2022-03-01 11:11:04 -0500

    created getStaticProps function in search component

--- pages/search.js
+++ pages/search.js
@@ -7,7 +7,7 @@ import Property from '../components/Property';
 import SearchFilters from '../components/SearchFilters';
 import noresult from '../assets/images/noresult.svg'
 
-const Search = () => {
+const Search = ({ properties }) => {
     const [searchFilters, setSearchFilters] = useState(false);
     const router = useRouter();
 
@@ -33,15 +33,37 @@ const Search = () => {
                 Properties {router.query.purpose}
             </Text>
             <Flex flexWrap="wrap">
-                {[].map((property) => <Property property={property} key={property.id} />)}
+                {properties.map((property) => <Property property={property} key={property.id} />)}
             </Flex>
-            {[].length === 0 && (
+            {properties.length === 0 && (
                 <Flex justifyContent="center" alignItems="center" flexDirection="column" marginTop="5" marginBottom="5">
                     <Image alt="no result" src={noresult} />
+                    <Text fontSize="2xl" marginTop="3">No Results Found</Text>
                 </Flex>
             )}
         </Box>
     )
+}
+
+export async function getServerSideProps({ query }) {
+    const purpose = query.purpose || 'for-rent';
+    const rentFrequency = query.rentFrequency || 'yearly';
+    const minPrice = query.minPrice || '0';
+    const maxPrice = query.maxPrice || '1000000';
+    const roomsMin = query.roomsMin || '0';
+    const bathsMin = query.bathsMin || '0';
+    const sort = query.sort || 'price-desc';
+    const areaMax = query.areaMax || '35000';
+    const locationExternalIDs = query.locationExternalIDs || '5002';
+    const categoryExternalID = query.categoryExternalID || '4';
+
+    const data = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`);
+
+    return {
+        props: {
+            properties: data?.hits,
+        },
+    };
 }
 
 export default Search;
