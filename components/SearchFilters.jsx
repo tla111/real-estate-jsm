f812ad03b648fcfb5eaaa8d74c9775c3eff0cd5f
Tim La <tlamarketing111@gmail.com>
2022-03-01 11:18:57 -0500

    created and added filter data

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -5,10 +5,12 @@ import { MdCancel } from 'react-icons/md';
 import Image from 'next/image';
 
 const SearchFilters = () => {
+    const [filters, setFilters] = useState({});
+
     return (
-        <div>
-            SearchFilters
-        </div>
+        <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
+
+        </Flex>
     )
 }
 
