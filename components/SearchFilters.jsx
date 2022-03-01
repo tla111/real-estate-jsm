2dcc446761d8f3f76650e0d136545880d6846997
Tim La <tlamarketing111@gmail.com>
2022-03-01 11:37:57 -0500

    successfully able to filter properties by different filter properties & values

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -8,9 +8,19 @@ import { filterData, getFilterValues } from '../utils/filterData';
 
 const SearchFilters = () => {
     const [filters, setFilters] = useState(filterData);
+    const router = useRouter();
 
-    const searchProperties = ({ filterValues }) => {
+    const searchProperties = (filterValues) => {
+        const path = router.pathname;
+        const { query } = router;
 
+        const values = getFilterValues(filterValues);
+
+        values.forEach((item) => {
+            query[item.name] = item.value;
+        })
+
+        router.push({ pathname: path, query })
     }
 
     return (
