891212723524c4833b30dfc8b02a653a3f3314f6
Tim La <tlamarketing111@gmail.com>
2022-03-02 09:20:37 -0500

    able to receive props and destructured propertyDetails

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -6,8 +6,18 @@ import millify from 'millify';
 
 import { baseUrl, fetchApi } from '../../utils/fetchApi';
 
-const PropertyDetails = () => (
+const PropertyDetails = ({ propertyDetails: { price, rentFrequency, rooms, title, baths, area, agency, isVerified, description, type, purpose, furnishingStatus, amenities, photos } }) => (
     <Box maxWidth={"1000px"} margin="auto" p="4">
 
     </Box>
 )
+
+export async function getServerSideProps({ params: { id } }) {
+    const data = await fetchApi(`${baseUrl}/properties/detail?externalID=${id}`);
+
+    return {
+        props: {
+            propertyDetails: data
+        }
+    }
+}
